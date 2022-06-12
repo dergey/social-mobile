6be6b6d68cbf54bfe49a02cc82e1d2ccc2ad3527
Sergey Zhuravlev <dergey@gmail.com>
2022-06-12 20:45:57 +0300

    Added a layout for ProfileActivity.

---
+++ app/src/main/java/com/sergey/zhuravlev/mobile/social/ui/profile/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.sergey.zhuravlev.mobile.social.ui.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sergey.zhuravlev.mobile.social.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
