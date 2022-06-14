840b3e7853c7b5b5877668460d253fdb793a94e1
Sergey Zhuravlev <dergey@gmail.com>
2022-06-14 23:23:53 +0300

    Expanded the endpoint for the profile

--- app/src/main/java/com/sergey/zhuravlev/mobile/social/client/api/ProfileEndpoints.java
+++ app/src/main/java/com/sergey/zhuravlev/mobile/social/client/api/ProfileEndpoints.java
@@ -3,6 +3,7 @@ package com.sergey.zhuravlev.mobile.social.client.api;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.sergey.zhuravlev.mobile.social.dto.PageDto;
 import com.sergey.zhuravlev.mobile.social.dto.message.MessageDto;
+import com.sergey.zhuravlev.mobile.social.dto.profile.ProfileDetailDto;
 import com.sergey.zhuravlev.mobile.social.dto.profile.ProfileDto;
 
 import retrofit2.http.GET;
@@ -10,6 +11,16 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ProfileEndpoints {
+
+
+    @GET("/api/profile")
+    ListenableFuture<ProfileDetailDto> getCurrentUserProfile();
+
+    @GET("/api/friend")
+    ListenableFuture<PageDto<ProfileDto>> getCurrentUserFriends(@Query(value = "page") Integer page,
+                                                                @Query(value = "size") Integer size);
+
+
 
     @GET("/api/profile/{username}/friend")
     ListenableFuture<PageDto<ProfileDto>> getProfileFriends(@Path("username") String username,
