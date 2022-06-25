6dae7315c4be008f26f664258378db8a62537742
Sergey Zhuravlev <dergey@gmail.com>
2022-06-25 23:26:17 +0300

    Added ProfileSettingActivity.

---
+++ app/src/main/java/com/sergey/zhuravlev/mobile/social/ui/profile/ProfileSettingActivity.java
@@ -0,0 +1,16 @@
+package com.sergey.zhuravlev.mobile.social.ui.profile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sergey.zhuravlev.mobile.social.R;
+
+public class ProfileSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_setting);
+    }
+}
