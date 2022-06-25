f02a02c38e3d5907f04a7c07eaf2d914d64a4957
Sergey Zhuravlev <dergey@gmail.com>
2022-06-26 00:55:52 +0300

    Added EmailConfirmActivity.

---
+++ app/src/main/java/com/sergey/zhuravlev/mobile/social/ui/registration/EmailConfirmActivity.java
@@ -0,0 +1,16 @@
+package com.sergey.zhuravlev.mobile.social.ui.registration;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sergey.zhuravlev.mobile.social.R;
+
+public class EmailConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_confirm);
+    }
+}
