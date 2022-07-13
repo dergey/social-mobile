05b24ca8fcda5491b76e605234e22219ec708345
Sergey Zhuravlev <dergey@gmail.com>
2022-07-13 14:00:50 +0300

    Added an ActionBar where needed.

--- app/src/main/java/com/sergey/zhuravlev/mobile/social/MainActivity.java
+++ app/src/main/java/com/sergey/zhuravlev/mobile/social/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity implements FragmentCallable 
                 R.id.navigation_chats, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-       // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
